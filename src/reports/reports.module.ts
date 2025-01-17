0298bf91a540d9a436959201e68f7bf69272dafd
jyothi-aadvi <jyothi@aadvisolutions.com>
2025-01-17 14:50:53 +0100

    reports

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -4,10 +4,9 @@ import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 import { Report } from './reports.entity';
 
-
 @Module({
   imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
 })
 export class ReportsModule {}
